4fed66342bd3afeb64d34daf1701925a03068995
Kyle Van Vleck <kylevanvleck@gmail.com>
2016-11-28 23:54:25 -0800

    initial folder architecture

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+
+gulp.task('watch', function(){
+
+  browserSync.init({
+    notify: false,  // disables notification box in browser of successful sync
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  watch('./app/index.html', function(){
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function(){
+    gulp.start('cssInject');
+  });
+
+});
+
+gulp.task('cssInject', ['styles'], function(){
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,22 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssvars = require('postcss-simple-vars');
-var nested = require('postcss-nested');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+
+
+
+/* EXAMPLE TASKS
+
+// example default gulp task -  run with "gulp" command
 
 gulp.task('default', function(){
   console.log("Default gulp task executed!");
 });
 
-gulp.task('whatever', function(){
+// example gulp task - run with "gulp html" command
+
+gulp.task('html', function(){
   console.log("Whatever...");
 });
 
-gulp.task('styles', function(){
-  return gulp.src('./app/styles/**/*.css')
-    .pipe(postcss([cssvars, nested, autoprefixer]))
-    .pipe(gulp.dest('./dist/styles'));
-});
+*/
 
-gulp.task('watch', function(){
-
-  watch('./index.html', function(){
-    gulp.start('whatever');
-  });
-
-  watch('./app/styles/**/*.css', function(){
-    gulp.start('styles');
-  });
-
-});
